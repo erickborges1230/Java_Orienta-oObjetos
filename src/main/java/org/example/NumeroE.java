157274c38a727af50565bf173dcdff9299ad1f2f
Erick Borges <erickborges1230@gmail.com>
2025-07-15 21:49:23 -0300

    first commit

--- src/main/java/org/example/NumeroE.java
+++ src/main/java/org/example/NumeroE.java
@@ -13,8 +13,28 @@ public class NumeroE {
         x = scanner.nextDouble();
 
         for (int i = 1; i <= 30; i++)
-            ex = ex + (Math.pow(x, i))/i; //x elevado a I divido por I
-        System.out.println("O valor de e ( " +x+ " ) é = "+ex);
+            ex = ex + (Math.pow(x, i)) / fatorialModificado(i); //x elevado a I divido por I
+        System.out.println("O valor de e ( " + x + " ) é = " + ex);
         scanner.close();
+    }
+
+    //calculando o fatorial de um número
+    public static int fatorial(int i)
+    {
+        int resultado = 1;
+        for (int j = 1; j <= i; i++) {
+            resultado *= j;
+        }
+        return resultado;
+    }
+    public static double fatorialModificado(int i)
+    {
+        return (switch (i){
+            case 0,1 -> 1;
+            default -> {
+                double fatorial = 1 * fatorialModificado(i-1);
+                yield fatorial;
+            }
+        });
     }
 }
